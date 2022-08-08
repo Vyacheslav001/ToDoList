29adfc4857c29928c4165dbfe37c35fd46505d8d
Vyacheslav001 <leo-slava@mail.ru>
2022-08-08 20:14:49 +0300

    Промежуточная заливка 1

---
+++ app/src/main/java/com/example/todolist/TodoItemAdapter.kt
@@ -0,0 +1,14 @@
+package com.example.todolist
+
+import androidx.recyclerview.widget.RecyclerView
+import com.example.todolist.fragments.TodoItem
+
+class TodoItemAdapter : RecyclerView.Adapter<TodoItemAdapter.TodoItemHolder> {
+
+    class TodoItemHolder() : RecyclerView.ViewHolder() {
+        fun bind(todoItem: TodoItem) {
+
+        }
+    }
+
+}
